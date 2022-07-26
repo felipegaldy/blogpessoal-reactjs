ac397d7496acfdb1ed5df0e4bc6433dc2813b127
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-26 18:39:43 -0300

    estilizando componente com css puro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/home/home';
 
 let nome = 'Felipe'
 function App() {
   return (
-    <h1>{nome}</h1>
+    <>
+    <Home/>
+    </>
   );
 }
 
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://pbs.twimg.com/media/DqMnM7kWoAAjYjg.jpg"  alt="imagem multi tarefa" className="img"/>
+        </>
+    );
+}
+
+export default Home;
