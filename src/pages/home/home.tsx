ffecbcbfb6c143787c1fe3231debc6a3f65bbb4a
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-27 11:26:48 -0300

    grid material ui na pratica

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./home.css";
+import { Grid, Paper } from "@material-ui/core";
 
 
 function Home() {
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://pbs.twimg.com/media/DqMnM7kWoAAjYjg.jpg"  alt="imagem multi tarefa" className="img"/>
+        <Grid container spacing={2} >
+            <Grid item xs={12} sm={8}>
+                <Paper style={{height:"100vh", background:"lightblue"}}   />
+            </Grid>
+            <Grid item container spacing={2} direction="column" xs={12} sm={4}>
+                <Grid item>
+                    <Paper style={{height:"49vh", background:"orange"}} />
+                </Grid>
+                <Grid item>
+                    <Paper style={{height: "49vh", background:"green"}} />
+                </Grid>
+            </Grid>    
+        </Grid>
         </>
     );
 }
