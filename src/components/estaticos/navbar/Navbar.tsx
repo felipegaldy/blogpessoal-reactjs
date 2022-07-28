4112b88c916d45c146116d3fe03ede04633d4751
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-28 07:14:29 -0300

    adicionando icones no navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-
+import HomeIcon from "@material-ui/icons/Home";
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import CategoryIcon from '@material-ui/icons/Category';
+import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 
 function Navbar() {
@@ -10,34 +14,34 @@ function Navbar() {
         <Toolbar variant="dense" style={{backgroundColor: "#222", display: "flex", justifyContent: "space-between"}}>
           <Box style={{cursor: "pointer"}}>
             <Typography variant="h5" color="inherit">
-              BlogPessoal
+              <img src="../../../assets/images/DSU_Developer.png" alt="logo" width="40px" height="30px" />
             </Typography>
           </Box>
           <Box display="flex" >
             <Box mx={1} style={{cursor: "pointer"}}>
               <Typography variant="h6" color="inherit">
-                HOME
+              <HomeIcon color="inherit" fontSize="large" /> {/*home*/}
               </Typography>
             </Box>
             <Box mx={1} style={{cursor: "pointer"}}>
               <Typography variant="h6" color="inherit">
-                POSTAGENS
+                <PostAddIcon color='inherit' fontSize="large" /> {/*postagens*/} 
               </Typography>
             </Box>
             <Box mx={1} style={{cursor: "pointer"}}>
               <Typography variant="h6" color="inherit">
-                TEMAS
+                <CategoryIcon color="inherit" fontSize="large" /> {/*TEMAS*/}
               </Typography>
             </Box>
             <Box mx={1} style={{cursor: "pointer"}}>
               <Typography variant="h6" color="inherit">
-                CADASTRAR TEMAS
+                <CreateNewFolderIcon color="inherit" fontSize="large" />{/*CADASTRAR TEMAS*/}
               </Typography>
             </Box>
 
             <Box mx={1} style={{cursor: "pointer"}}>
               <Typography variant="h6" color="inherit">
-                LOGOUT
+                <ExitToAppIcon color="inherit" fontSize="large" />{/*LOGOUT*/}
               </Typography>
             </Box>
           </Box>
