209397b16206ff85308f07e5e2d0893e2388903d
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-29 16:02:38 -0300

    refatorando estilos login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,17 +6,17 @@ import { Link } from "react-router-dom";
 function Login() {
   return (
     <>
-      <Grid container direction='row' justifyContent='center' alignItems='center' style={{ backgroundColor: "#222", color: "white", minHeight:"70vh"}}>
+      <Grid container direction='row' justifyContent='center' alignItems='center' >{/*style={{ backgroundColor: "#222", color: "white", minHeight:"70vh"}} */}
         <Grid alignItems='center' xs={6}>
           <Box paddingX={20} >
             <form>
               <Typography
                 variant="h3"
                 gutterBottom
-                color="white"
+                color="textPrimary"
                 component="h3"
                 align="center"
-                style={{ fontWeight: "bold" }}
+                className="textos1"
               >
                 Entrar
               </Typography>
@@ -34,20 +34,14 @@ function Login() {
               <Box marginRight={1}>
                 <Typography variant='subtitle1' gutterBottom alignItems='center' >Não tem uma conta ?</Typography>
               </Box>
-              <Typography variant='subtitle1' gutterBottom alignItems='center' style={{fontWeight: 'bold'}} >Cadastre-se</Typography>
+              <Typography variant='subtitle1' gutterBottom alignItems='center' className="textos1" >Cadastre-se</Typography>
             </Box>
           </Box>
         </Grid>
-        {/*
-        <Grid xs={6} style={{
-          backgroundImage: `url(https://i0.wp.com/blog.mcmattys.com/wp-content/uploads/2020/03/1584183927_802_unDraw-open-source-illustrations-built-in-SVG-free-and-without-attribution.png?w=1080&ssl=1)`,
-          backgroundRepeat: 'no-repeat',
-          width: '100vh',
-          minHeight: '100vh',
-          backgroundSize:'cover',
-          backgroundPosition: 'center',
-        }} ></Grid> 
-        */}
+        
+        <Grid xs={6} className="imagem" >
+        </Grid> 
+        
       </Grid>
     </>
   );
