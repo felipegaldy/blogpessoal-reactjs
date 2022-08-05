0816cfc7fc4240322c146d941648ceaec565bc72
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-08-05 18:31:45 -0300

    post e put postagem

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,30 +1,50 @@
 import { Box, Grid, Typography, Button } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import useLocalStorage from "react-use-localstorage";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
 import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
 import './home.css';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home() {
+
+    let navigate = useNavigate();
+
+    const [token, setToken] = useLocalStorage('token')
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+
+        }
+    }, [token])
+
   return (
       <>
           <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-              <Grid alignItems="center" item xs={6}>
-                  <Box paddingX={20} >
-                      <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                      <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
-                  </Box>
-                  <Box display="flex" justifyContent="center">
-                      <Box marginRight={1}>
-                      </Box>
-                      <Button variant="outlined" className='botao'>Ver Postagens</Button>
-                  </Box>
-              </Grid>
-              <Grid item xs={6} >
-                  <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
-              </Grid>
-              <Grid xs={12} className='postagens'>
-                  <TabPostagem />
-              </Grid>
-          </Grid>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalPostagem />
+                        </Box>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                        </Link>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
+                </Grid>
+            </Grid>
       </>
   );
 }
